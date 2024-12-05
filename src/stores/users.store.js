7028b6c2e9e129086487d8c68b4368cae36e0bff
mysoyul <vega2k_@naver.com>
2024-12-05 17:23:56 +0900

    RegisterForm add

---
+++ src/stores/users.store.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+import { fetchWrapper } from '@/helpers';
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useUsersStore = defineStore('users', {
+    state: () => ({
+        user: {}
+    }),
+    actions: {
+        async register(user) {
+            await fetchWrapper.post(`${baseUrl}/auth/register`, user);
+        },
+
+    }
+});
