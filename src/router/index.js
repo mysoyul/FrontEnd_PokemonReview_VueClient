0506b3cf316085184d86db6c1e399fe284e30609
mysoyul <vega2k_@naver.com>
2024-12-05 16:48:10 +0900

    rounter index/account.routes 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore, useAlertStore } from '@/stores';
+import { Home } from '@/views';
+import accountRoutes from './account.routes';
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass: 'active',
   routes: [
+    { path: '/', component: Home },
+    { ...accountRoutes },
+    // 매칭이 되지 않는 URL로 접근 시 마지막 path인 "/:pathMatch(.*)*"로 적용된다.
+    { path: '/:pathMatch(.*)*', redirect: '/' }
+  ]
+});
 
-  ],
-})
+//router.beforeEach()를 사용하여 라우트 되기 이전에 전역 등록을 할 수 있다
+// to : 이동할 url 정보가 담긴 라우터 객체
+router.beforeEach(async (to) => {
+  // clear alert on route change
+  const alertStore = useAlertStore();
+  alertStore.clear();
 
-export default router
+  // redirect to login page if not logged in and trying to access a restricted page 
+  const publicPages = ['/account/login',];
+  const authRequired = !publicPages.includes(to.path);
+  const authStore = useAuthStore();
+
+  console.log('to.path ' + to.path)
+  console.log('authRequired ' + authRequired)
+  console.log('!authStore.user ' + !authStore.user)
+  console.log(authRequired && !authStore.user)
+
+  if (authRequired && !authStore.user) {
+    authStore.returnUrl = to.fullPath;
+    return '/account/login';
+  }
+});
